8c7b089bc20857a61e3a47eea2eec05ab8aaf926
TheNootLinja <thenootlinja@gmail.com>
2022-03-04 11:08:33 -0500

    Add finished code for project

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+let items = JSON.parse(localStorage.getItem('items')) || [];
+const clearButton = document.querySelector('.clear-button');
+const checkButton = document.querySelector('.check-button');
+const uncheckButton = document.querySelector('.uncheck-button');
 
 function addItem(e) {
   e.preventDefault();
@@ -10,20 +13,60 @@ function addItem(e) {
     done: false
   }
   items.push(item)
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
   this.reset();
-  console.table(items);
-  populateList(items, itemsList)
 }
 
 function populateList(items = [], itemsList) {
   const itemsHtml = items.map((item, i) => {
     return `
       <li>
-      <label for="">${item.name}</label>
+      <input type="checkbox" data-index="${i}" id="item${i}" ${item.done ? 'checked' : ''}/>
+      <label for="item${i}">${item.name}</label>
       </li>
     `;
   }).join('');
   itemsList.innerHTML = itemsHtml;
 }
 
-addItems.addEventListener('submit', addItem)
+function toggleDone(e) {
+  if(!e.target.matches('input')) return;
+  const el = e.target;
+  const elIndex = el.dataset.index;
+  items[elIndex].done = !items[elIndex].done;
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+// Uncheck All Button
+const clearAll = () => {
+  items = [];
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+};
+const checkAll = () => {
+  items.map(item => {
+    item.done = true;
+  });
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+};
+
+const uncheckAll = () => {
+  items.map(item => {
+    item.done = false;
+  });
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+};
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+clearButton.addEventListener('click', clearAll);
+uncheckButton.addEventListener('click', uncheckAll);
+checkButton.addEventListener('click', checkAll);
+
+populateList(items, itemsList);
+
+// Clear All Button
+// Check All Button
